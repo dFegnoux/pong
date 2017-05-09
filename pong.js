28d0358653443daa31830d663af1bf10d2d543a1
Denis Fegnoux <denis.fegnoux@gmail.com>
2017-05-09 18:01:36 +0200

    Add player's rackets

--- pong.js
+++ pong.js
@@ -2,12 +2,14 @@ window.onload = function() {
 	var options = {
 		netWidth: 5,
 		fieldBordersWidth: 5,
-		fieldPadding: 10
+		fieldPadding: 10,
+		racketHeight: 100,
+		racketWidth: 20
 	}
 
 	var canvas = document.getElementById('pong');
 	var ctx = canvas.getContext('2d');
-	
+
 	// Set canvas size
 	ctx.canvas.width  = window.innerWidth - 24;
 	ctx.canvas.height = window.innerHeight - 24;
@@ -20,9 +22,13 @@ window.onload = function() {
 	ctx.fillStyle = 'white';
 	ctx.fillRect(options.fieldPadding, options.fieldPadding, canvas.width - 20, options.fieldBordersWidth);
 	ctx.fillRect(options.fieldPadding, canvas.height - (10 + options.fieldBordersWidth), canvas.width - 20, options.fieldBordersWidth);
-	
+
 	// Field net
 	ctx.fillStyle = 'white';
 	ctx.fillRect((canvas.width/2) - (options.netWidth/2), options.fieldPadding, options.netWidth, canvas.height - (20 + options.fieldBordersWidth));
+
+	// Player's Racket
+	ctx.fillRect(options.fieldPadding, (canvas.height / 2) - (options.racketHeight / 2), options.racketWidth, options.racketHeight);
+	ctx.fillRect(canvas.width - options.fieldPadding - options.racketWidth, (canvas.height / 2) - (options.racketHeight / 2), options.racketWidth, options.racketHeight);
 
 }
